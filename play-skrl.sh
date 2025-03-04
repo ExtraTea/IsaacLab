9e2fbab432f8c7a62fd39367598466a47dee958d
Daisuke <d-takamatsu@g.ecc.u-tokyo.ac.jp>
2025-03-04 15:21:41 +0900

    added g1 playing shell code

---
+++ play-skrl.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+/isaaclab.sh -p scripts/reinforcement_learning/skrl/play.py --task Isaac-Velocity-Flat-G1-v0 --num_envs 32 --checkpoint /home/daisuke/IsaacLab/logs/skrl/g1_flat/2025-02-28_17-39-35_ppo_torch/checkpoints/best_agent.pt
