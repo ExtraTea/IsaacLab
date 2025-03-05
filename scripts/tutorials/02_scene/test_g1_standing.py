3ee163b9f90f4863fae29777c544448d43965259
Daisuke <d-takamatsu@g.ecc.u-tokyo.ac.jp>
2025-03-05 18:36:39 +0900

    added test func

---
+++ scripts/tutorials/02_scene/test_g1_standing.py
@@ -0,0 +1,59 @@
+from isaaclab.app import AppLauncher
+
+app_launcher = AppLauncher()
+simulation_app = app_launcher.app
+
+import torch
+import isaaclab.sim as sim_utils
+from isaaclab.assets import ArticulationCfg, AssetBaseCfg
+from isaaclab.scene import InteractiveScene, InteractiveSceneCfg
+from isaaclab.sim import SimulationContext
+from isaaclab.utils import configclass
+
+from isaaclab_assets import G1_CFG
+
+from isaacsim.core.api.robots.robot_view import RobotView
+
+
+@configclass
+class G1SceneCfg(InteractiveSceneCfg):
+    ground = AssetBaseCfg(prim_path="/World/defaultGroundPlane", spawn=sim_utils.GroundPlaneCfg())
+    dome_light = AssetBaseCfg(prim_path="/World/Light", spawn=sim_utils.DomeLightCfg(intensity=3000.0, color=(0.75, 0.75, 0.75)))
+    robot : ArticulationCfg = G1_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
+
+
+def run_simulator(sim: sim_utils.SimulationContext, scene: InteractiveScene):
+    robot = scene["robot"]
+    sim_dt = sim.get_physics_dt()
+    count = 0
+    while simulation_app.is_running():
+        if count % 500 == 0:
+            count = 0
+            root_state = robot.data.default_root_state.clone()
+            root_state[:, :3] += scene.env_origins
+            robot.write_root_pose_to_sim(root_state[:, :7])
+            robot.write_root_velocity_to_sim(root_state[:, 7:])
+            robot_view = RobotView(prim_paths_expr="/World/envs/env_.*/Robot")
+            # print(robot_view.initialize())
+            # print(robot_view.get_jacobians().shape)
+            scene.reset()
+            print("Resetting scene")
+        default_pos = robot.data.default_joint_pos.clone()
+        robot.set_joint_position_target(default_pos)
+        scene.write_data_to_sim()
+        sim.step()
+        count += 1
+        scene.update(sim_dt)
+
+def main():
+    sim_cfg = sim_utils.SimulationCfg(device="cpu")
+    sim = SimulationContext(sim_cfg)
+    sim.set_camera_view([2.5, 0.0, 4.0], [0.0, 0.0, 2.0])
+    scene_cfg = G1SceneCfg(num_envs=1, env_spacing=0.0)
+    scene = InteractiveScene(scene_cfg)
+    sim.reset()
+    run_simulator(sim, scene)
+
+if __name__ == "__main__":
+    main()
+    simulation_app.close()
