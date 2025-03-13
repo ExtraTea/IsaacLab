26de1055207f8a017c3bd4aa29204a5b9919216c
Daisuke <d-takamatsu@g.ecc.u-tokyo.ac.jp>
2025-03-13 18:12:03 +0900

    added feature

--- play-skrl.sh
+++ play-skrl.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/bash
-/isaaclab.sh -p scripts/reinforcement_learning/skrl/play.py --task Isaac-Velocity-Flat-G1-v0 --num_envs 32 --checkpoint /home/daisuke/IsaacLab/logs/skrl/g1_flat/2025-02-28_17-39-35_ppo_torch/checkpoints/best_agent.pt
+#./isaaclab.sh -p scripts/reinforcement_learning/skrl/play.py --task Isaac-Velocity-Flat-G1-v0 --num_envs 32 --checkpoint /home/daisuke/IsaacLab/logs/skrl/g1_flat/2025-03-11_17-14-46_ppo_torch/checkpoints/agent_500000.pt
+
+#!/bin/bash
+# xclipがインストールされていることを確認してください
+TEXT="./isaaclab.sh -p scripts/reinforcement_learning/skrl/play.py --task Isaac-Velocity-Flat-G1-v0 --num_envs 32 --checkpoint /home/daisuke/IsaacLab/logs/skrl/g1_flat/2025-"
+echo -n "$TEXT" | xclip -selection clipboard
+sleep 0.5
+# ここではペーストのために、Ctrl+Shift+V のショートカットを送信しています（ターミナルなど）
+xdotool key ctrl+shift+v
+
